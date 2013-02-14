01e422a787023b1eb68de586d35ca8da6cb6de0b
Petter Abrahamsen <petterabrahamsen91@hotmail.com>
2013-02-14 16:29:21 +0100

    Lab 2 svart

--- 4_5_2.c
+++ 4_5_2.c
@@ -3,6 +3,16 @@
 #include <unistd.h> /* fork */
 #include <sys/wait.h> /* waitpid */
 #include <sys/types.h> /* pid_t */
+
+/* Svar på Lab oppgave 4.5.2: både child og parent har samme globale variable
+men denne er forskjellig fra child til parent, så child teller g_ant fra 0->30
+og parent teller g_ant fra 0 -> 30. tallet øker hver gang i blir hundre tusen,
+så g_ant økes hver hundretusende runde i while loopen. Dette gjør at når 
+datamaskinen switcher mellom trådene så vil ikke den første tråden være ferdig
+enda med å telle. Det vil finnes en g_ant for hver tråd, så g_ant teller to 
+ganger til 30, en for hver tråd.*/
+
+
 int g_ant = 0; /* global declaration */
 void writeloop(char *text) {
   long i = 0;
