96939f09a04ec5299fe3ec25d478227bd5a6171f
Petter Abrahamsen <petterabrahamsen91@hotmail.com>
2013-02-15 16:36:54 +0100

    5.6.2 endringer

--- 5_6_2.c
+++ 5_6_2.c
@@ -10,29 +10,37 @@
 
 void *Producer(void *); /* Producer thread */
 void *Consumer(void *); /* Consumer thread */
-int stat = 1;
+
 sem_t empty;            /* empty: How many empty buffer slots */
 sem_t full;             /* full: How many full buffer slots */
 pthread_mutex_t mutex;  /* mutex: Mutex lock */
 int g_data[BUF_SIZE];     /* shared finite buffer  */
 
-int main(int N) {
+int main (int argc, char *argv[]) {
+  int N = atoi(argv[1]);
+  printf("%d", N);
   pthread_t pid[N], cid[N];
   sem_init(&empty, SHARED, BUF_SIZE);
   sem_init(&full, SHARED, 0);
   pthread_mutex_init(&mutex,0);
-
-  pthread_t *pid1 = &pid[0], *cid1 = &cid[0];
+  int id[N*2];
+  //  pthread_t *pid1 = &pid[0], *cid1 = &cid[0];
   printf("main started\n");
   int i;
   for(i = 0; i<N; i++)
   {
-    pthread_create(pid1++, NULL, Producer, (void*) &stat);stat++;
-    pthread_create(cid1++, NULL, Consumer, (void*) &stat);stat++;
+   
+    id[i*2] = i*2;
+    pthread_create(&pid[i], NULL, Producer, (void*) &id[i*2]);
+    id[(i*2)+1]=(i*2)+1;
+    pthread_create(&cid[i], NULL, Consumer, (void*) &id[(i*2)+1]);
+  }
+  for(i = 0; i<N; i++)
+  {
     pthread_join(pid[i], NULL);
     pthread_join(cid[i], NULL);
   }
-  
+   
   printf("main done\n");
 
   return 0;
@@ -41,7 +49,8 @@ int main(int N) {
 
 void *Producer(void *arg) {
   int semvalem,i=0,j;          /* semvalem: semaphore value of empty semaphore */
-  int *navn = (int*) arg;
+  int *x = (int*) arg;
+  int navn = *x;
   while(i < NUM_ITER) {
     //    usleep(rand()%MAX_MOD);    /* pretend to generate an item by a random wait*/
     sem_wait(&empty);
@@ -49,7 +58,7 @@ void *Producer(void *arg) {
     sem_getvalue(&empty, &semvalem);
     g_data[BUF_SIZE-(semvalem-1)]=1; /* put item in buffer */
     /* the following two lines just prints a bar showing current buffer fill */
-    j=BUF_SIZE; printf("(Producer %d, semaphore empty is %d) \t", *navn, semvalem);
+    j=BUF_SIZE; printf("(Producer %d, semaphore empty is %d) \t", navn, semvalem);
     while(j > semvalem) { j--; printf("="); } printf("\n");
     pthread_mutex_unlock(&mutex);
     sem_post(&full);
@@ -62,7 +71,8 @@ void *Producer(void *arg) {
 
 void *Consumer(void *arg) {
   int semvalfu,i=0,j;       /* semvalfu: semaphore value of full semaphore */
-  int *navn = (int*) arg;
+  int *x = (int*) arg;
+  int navn = *x;
   while(i < NUM_ITER) {
     //    usleep(rand()%MAX_MOD); /* pretend to generate an item by a random wait*/
     sem_wait(&full);
@@ -70,7 +80,7 @@ void *Consumer(void *arg) {
     sem_getvalue(&full, &semvalfu);
     g_data[semvalfu]=0;       /* remove item from buffer */
     /* the following two lines just prints a bar showing current buffer fill */
-    j=0; printf("(Consumer id: %d, semaphore full is %d)  \t", *navn, semvalfu);
+    j=0; printf("(Consumer %d, semaphore full is %d)  \t", navn, semvalfu);
     while(j < semvalfu) { j++; printf("="); } printf("\n");
     pthread_mutex_unlock(&mutex);
     sem_post(&empty);
